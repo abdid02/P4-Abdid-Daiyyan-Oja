a1b1395ebad65442571db5432f2dd25257d76cc1
Abdid daiyyan <64628607+abdid02@users.noreply.github.com>
2020-05-17 23:19:21 +0700

    Create Jobsheet 2 C.cpp
    mengisi dan menampilkan 
    element array (dinamis)

---
+++ Jobsheet 2 C.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+ main(){
+ int x[4];
+ printf("masukkan nilai 1 : ");
+ scanf ("%i",&x[0]);
+ printf("masukkan nilai 2 : ");
+ scanf ("%i",&x[1]);
+ printf("masukkan nilai 3 : ");
+ scanf ("%i",&x[2]);
+ printf("masukkan nilai 4 : ");
+ scanf("%i",&x[3]);
+ printf("\n");
+ 
+ printf("nilai ke 1 =  %i\n",x[0]);
+ printf("nilai ke 2 =  %i\n",x[1]);
+ printf("nilai ke 3 =  %i\n",x[2]);
+ printf("nilai ke 4 =  %i\n",x[3]);
+ return 0;
+}
