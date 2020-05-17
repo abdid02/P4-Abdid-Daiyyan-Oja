3be740bfc8903ec62a519bbc1099805ae308b48e
Abdid daiyyan <64628607+abdid02@users.noreply.github.com>
2020-05-17 23:17:11 +0700

    Create Jobsheet 1 C.cpp
    membuat array satu dimensi

---
+++ Jobsheet 1 C.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+ printf("Abdid Daiyyan Oja-F1B019002/KELOMPOK 1 \n");
+
+ int nim[4]= {8,2,3,1};
+ 
+ printf ("Nim ke-1 = %i\n",nim[0]);
+ printf ("Nim ke-2 = %i\n",nim[1]);
+ printf ("Nim ke-3 = %i\n",nim[2]);
+ printf ("Nim ke-4 = %i\n",nim[3]);
+
+}
