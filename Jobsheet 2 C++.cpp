31a5553085e81fec5e2e1c495361d27ff98ae32b
Abdid daiyyan <64628607+abdid02@users.noreply.github.com>
2020-05-17 23:20:18 +0700

    Create Jobsheet 2 C++.cpp
    mengisi dan menampilkan 
    element array (dinamis)

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int x[4];
+ cout<<"masukkan nilai 1 : ";
+ cin>>x[0];
+ cout<<"masukkan nilai 2 : ";
+ cin>>x[1];
+ cout<<"masukkan nilai 3 : ";
+ cin>>x[2];
+ cout<<"masukkan nilai 4 : ";
+ cin>>x[3];
+ cout<<endl;
+ 
+ cout<<"nilai ke 1 =  "<<x[0]<<endl;
+ cout<<"nilai ke 2 =  "<<x[1]<<endl;
+ cout<<"nilai ke 3 =  "<<x[2]<<endl;
+ cout<<"nilai ke 4 =  "<<x[3]<<endl;
+ system("PAUSE");
+}
